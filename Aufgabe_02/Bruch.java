18507602446ed5f82056d3d9d58c16dae33c839d
Jakob <jakob.schulz2004@gmail.com>
2024-04-07 17:26:48 +0200

    Ansatz Aufgabe 2

---
+++ Aufgabe_02/Bruch.java
@@ -0,0 +1,79 @@
+public class Bruch {
+    private int nenner;
+    private int zaehler;
+
+    int ggT(int a, int b) {
+        a = Math.abs(a); //Der Betrag wird erstellt, damit auch negative Zahlen behandelt werden können
+        b = Math.abs(b);
+        if (b > a) {
+            return ggT(b, a);
+        } else {
+            if (b == 0) {
+                return a;
+            } else {
+                return ggT(b, a % b);
+            }
+        }
+    }
+
+    void kuerzen() {
+        // kuerzen der Zaehler und der Nenner
+        int ggt = ggT(zaehler, nenner);
+        nenner = nenner / ggt;
+        zaehler = zaehler / ggt;
+    }
+
+    public Bruch(int zaehler, int nenner) {
+        this.zaehler = zaehler;
+        //Ist der Nenner 0 wird eine Exception geworfen
+        if(nenner == 0)
+        {
+            throw new ArithmeticException("Der Nenner ist 0. Es ist nicht moeglich durch 0 zu teilen");
+        }
+        else
+        {
+        this.nenner = nenner;
+        }
+    }
+
+    public int getNenner() {
+        return nenner;
+    }
+
+    public int getZaehler() {
+        return zaehler;
+    }
+
+    // Gibt den Bruch als String zurueck
+    public String get() {
+        return zaehler + "/" + nenner;
+    }
+
+    public void mal(Bruch b) {
+        // Zaehler und Nenner werden multipliziert
+        zaehler = zaehler * b.getZaehler();
+        nenner = nenner * b.getNenner();
+        kuerzen();
+    }
+
+    public void geteilt(Bruch b) {
+        // Der Bruch wird mit dem Kehrbruch multipliziert
+        Bruch kehrBruch = new Bruch(b.nenner, b.zaehler);
+        this.mal(kehrBruch);
+        kuerzen();
+    }
+
+    public void plus(Bruch b) {
+        // Die Brueche muessen erweitert werden, damit sie einen gemeinsamen Nenner haben. Dann werden sie gekuerzt.
+        zaehler = zaehler * b.nenner + b.zaehler * nenner;
+        nenner = nenner * b.nenner;
+        kuerzen();
+    }
+
+    public void minus(Bruch b) {
+        // Die Brueche muessen erweitert werden, damit sie einen gemeinsamen Nenner haben. Dann werden sie gekuerzt.
+        zaehler = zaehler * b.nenner - b.zaehler * nenner;
+        nenner = nenner * b.nenner;
+        kuerzen();
+    }
+}
---
+++ Aufgabe_02/BruchTest.java
@@ -0,0 +1,44 @@
+public class BruchTest {
+    public static void main(String[] args) {
+        Bruch x = new Bruch(1, 2);
+        Bruch y = new Bruch(2, 3);
+        y.plus(x);
+        System.out.println("y = " + y.get());
+        x.minus(y);
+        System.out.println("x = " + x.get());
+        Bruch a = new Bruch(17, 35);
+        a.mal(y);
+        System.out.println("a = " + a.get());
+        Bruch b = new Bruch(12, 34);
+        b.geteilt(a);
+        System.out.println("b = " + b.get());
+        // Erwarteter Output: y = 7/6, x = -2/3, a = 17/30, b = 180/289
+
+        // Teilaufgabe 2 (Tests):
+        Bruch summe = new Bruch(1, 1);
+        for (int i = 2; i <= 12; i++) {
+            summe.plus(new Bruch(1, i));
+        }
+        System.out.println("Teilaufgabe 2 (12 Wdh.): " + summe.get());
+        // Erwartetes Ergebnis: 86021/27720
+        Bruch e = e(8);
+        System.out.println("Die Eulersche Zahl e angenaehert: " + e.get());
+    }
+
+    //Ab n=9 annaeherung falsch, weil Zahlen zu groß werden. Static wird benoetigt, weil die main-Methode auch static ist
+    private static Bruch e(int n) {
+        Bruch summe = new Bruch(1, 1);
+        for (int anzahl = 1; anzahl <= n; anzahl++) {
+            //Berechnen des Nenners, also der Fakultaet
+            int fakultaet = anzahl;
+            int i = anzahl;
+            while (i > 2) {
+                fakultaet = fakultaet * (i - 1);
+                i = i - 1;
+            }
+            //Aufaddieren der einzelnen Brueche
+            summe.plus(new Bruch(1, fakultaet));
+        }
+        return summe;
+    }
+}
